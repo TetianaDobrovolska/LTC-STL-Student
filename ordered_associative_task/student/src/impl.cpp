4348dd6765a8866fd8317120f5910adde2e3c642
Tetiana Dobrovolska <TDobrovolska@luxoft.com>
2020-12-18 14:31:43 +0200

    Review changes

--- ordered_associative_task/student/src/impl.cpp
+++ ordered_associative_task/student/src/impl.cpp
@@ -2,19 +2,7 @@
 
 size_t convertMarkTo5PointSystem(size_t markIn100Point)
 {
-    enum Point
-    {
-        ONE = 1,
-        TWO,
-        THREE,
-        FOUR,
-        MAX_POINT
-    };
-    const std::map <size_t, size_t> converter {{29, static_cast<size_t>(Point::ONE)},
-                                               {49, static_cast<size_t>(Point::TWO)},
-                                               {69, static_cast<size_t>(Point::THREE)},
-                                               {89, static_cast<size_t>(Point::FOUR)},
-                                               {90, static_cast<size_t>(Point::MAX_POINT)}};
-    const auto result = converter.lower_bound(markIn100Point);
-    return converter.end() != result ? result->second : static_cast<size_t>(Point::MAX_POINT);
+    const std::map <size_t, size_t> converter {{0, 1}, {30, 2}, {50, 3}, {70, 4}, {90, 5}};
+    const auto result = --(converter.upper_bound(markIn100Point));
+    return result->second;
 }
